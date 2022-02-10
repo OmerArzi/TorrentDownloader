ee3e238da584d4946a6c408592e637b87327668c
Omer Arzi <arziomer@gmail.com>
2022-02-10 21:09:45 +0200

    File organizer added

--- Main.py
+++ Main.py
@@ -111,5 +111,5 @@ def main():
 
 
 if __name__ == '__main__':
-    organize_single_series("ousama ranking", "D:\\Shows\\")
-    #main()
+    #organize_single_series("ousama ranking", "D:\\Shows\\")
+    main()
